04e9e9f83fcd40ae05da9ec7c2aea67f4f5c58a6
Pompateh <newstalgia.orders@gmail.com>
2025-06-08 20:31:45 +0700

    fix

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
