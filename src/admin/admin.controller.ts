beea83ab824de0a7018b5befe02f34db15931edf
Pompateh <newstalgia.orders@gmail.com>
2025-06-09 19:52:28 +0700

    fixing error

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, Req, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { AdminService } from './admin.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('admin')
 @UseGuards(JwtAuthGuard)
@@ -13,14 +14,70 @@ export class AdminController {
     return this.adminService.getDashboardStats();
   }
 
+  @Get('studios')
+  async getStudios() {
+    return this.adminService.getStudios();
+  }
+
+  @Post('studios')
+  async createStudio(@Body() data: any) {
+    return this.adminService.createStudio(data);
+  }
+
+  @Put('studios/:id')
+  async updateStudio(@Param('id') id: string, @Body() data: any) {
+    return this.adminService.updateStudio(id, data);
+  }
+
+  @Delete('studios/:id')
+  async deleteStudio(@Param('id') id: string) {
+    return this.adminService.deleteStudio(id);
+  }
+
   @Get('projects')
   async getProjects() {
     return this.adminService.getProjects();
   }
 
-  @Get('studios')
-  async getStudios() {
-    return this.adminService.getStudios();
+  @Post('projects')
+  async createProject(@Body() data: any) {
+    return this.adminService.createProject(data);
+  }
+
+  @Put('projects/:id')
+  async updateProject(@Param('id') id: string, @Body() data: any) {
+    return this.adminService.updateProject(id, data);
+  }
+
+  @Delete('projects/:id')
+  async deleteProject(@Param('id') id: string) {
+    return this.adminService.deleteProject(id);
+  }
+
+  @Get('posts')
+  async getPosts() {
+    return this.adminService.getPosts();
+  }
+
+  @Post('posts')
+  async createPost(@Body() data: any) {
+    return this.adminService.createPost(data);
+  }
+
+  @Put('posts/:id')
+  async updatePost(@Param('id') id: string, @Body() data: any) {
+    return this.adminService.updatePost(id, data);
+  }
+
+  @Delete('posts/:id')
+  async deletePost(@Param('id') id: string) {
+    return this.adminService.deletePost(id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.adminService.uploadFile(file);
   }
 
   @Get('orders')
@@ -31,11 +88,6 @@ export class AdminController {
   @Get('users')
   async getUsers() {
     return this.adminService.getUsers();
-  }
-
-  @Get('posts')
-  async getPosts() {
-    return this.adminService.getPosts();
   }
 
   @Get('newsletter')
